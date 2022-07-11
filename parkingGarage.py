f679f628d6614fd42a9f9f1e20d00226655f6310
Brandon Nazario <brandon.nazario21@gmail.com>
2022-07-10 22:34:11 -0400

    Parking garage skeleton?

---
+++ parkingGarage.py
@@ -0,0 +1,21 @@
+# Start Your Code here
+
+class parkingGarage():
+    
+
+    def __init__(self, ticket, parking, remove_parking, price = 35):
+        self.ticket = price
+        self.add_park = parking []
+        self.add_ticket = ticket []
+        self.remove_park = remove_parking
+        
+        pass
+
+    def takeTicket(self):
+        pass
+
+    def payForParking(self):
+        pass
+
+    def leaveGarage(self):
+        pass
